93cb5acdfcabae024c62122b9742755d87ac93ee
alexmaryin <java73@yandex.ru>
2025-04-17 21:28:00 +0300

    Search now is working with pagination. Need to fix errors handling.

--- composeApp/src/commonMain/kotlin/ru/alexmaryin/di/modules.kt
+++ composeApp/src/commonMain/kotlin/ru/alexmaryin/di/modules.kt
@@ -1,7 +1,5 @@
 package ru.alexmaryin.di
 
-import androidx.paging.Pager
-import androidx.paging.PagingConfig
 import androidx.sqlite.driver.bundled.BundledSQLiteDriver
 import org.koin.core.module.Module
 import org.koin.core.module.dsl.singleOf
@@ -16,7 +14,6 @@ import ru.alexmaryin.news.data.remote_api.NewsPagingSource
 import ru.alexmaryin.news.data.remote_api.RemoteNewsDataSource
 import ru.alexmaryin.news.data.repository.DefaultSpaceNewsRepository
 import ru.alexmaryin.news.domain.SpaceNewsRepository
-import ru.alexmaryin.news.domain.models.Article
 import ru.alexmaryin.news.ui.SelectedArticleViewModel
 import ru.alexmaryin.news.ui.article_details.ArticleDetailsViewModel
 import ru.alexmaryin.news.ui.news_list.NewsListViewModel
@@ -34,17 +31,6 @@ val sharedModule = module {
             .build()
     }
     single { get<ArticlesDatabase>().dao }
-
-    single {
-        Pager<Int, Article>(
-            config = PagingConfig(
-                pageSize = RemoteNewsDataSource.DEFAULT_LIMIT,
-                initialLoadSize = RemoteNewsDataSource.DEFAULT_LIMIT,
-                enablePlaceholders = false
-            ),
-            pagingSourceFactory = { NewsPagingSource(get()) }
-        )
-    }
 
     viewModelOf(::NewsListViewModel)
     viewModelOf(::SelectedArticleViewModel)
--- composeApp/src/commonMain/kotlin/ru/alexmaryin/news/data/remote_api/KtorRemoteNewsDataSource.kt
+++ composeApp/src/commonMain/kotlin/ru/alexmaryin/news/data/remote_api/KtorRemoteNewsDataSource.kt
@@ -1,15 +1,28 @@
 package ru.alexmaryin.news.data.remote_api
 
-import app.cash.paging.Pager
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
 import app.cash.paging.PagingData
+import io.ktor.client.HttpClient
 import kotlinx.coroutines.flow.Flow
 import ru.alexmaryin.news.domain.models.Article
 
 class KtorRemoteNewsDataSource(
-    private val pager: Pager<Int, Article>
+    private val httpClient: HttpClient
 ) : RemoteNewsDataSource {
     override fun searchNews(
         query: String,
         limit: Int
-    ): Flow<PagingData<Article>> = pager.flow
+    ): Flow<PagingData<Article>> {
+        val pager = Pager<Int, Article>(
+            config = PagingConfig(
+                pageSize = RemoteNewsDataSource.DEFAULT_LIMIT,
+                initialLoadSize = RemoteNewsDataSource.DEFAULT_LIMIT,
+                enablePlaceholders = false
+            ),
+            pagingSourceFactory = { NewsPagingSource(httpClient, query) }
+        )
+
+        return pager.flow
+    }
 }
